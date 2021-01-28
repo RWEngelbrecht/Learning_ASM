52fe9d7035e9a2def727826ea0a2e9c17808e140
RWEngelbrecht <rigardt@brechtburg.co.za>
2021-01-28 14:24:11 +0200

    added test for bzero_asm

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 
 int putchar_asm(int s);
+void bzero_asm(void *s, size_t n);
 
 int main(void)
 {
@@ -18,6 +19,13 @@ int main(void)
     putchar(str);
 	putchar_asm('\n');
     putchar_asm(str);
+	putchar_asm('\n');
+	char z[7] = "hellow";
+	printf("ft_bzero\n");
+	bzero_asm(z, 0);
+	printf("  bzero_asm(\"hellow\", 0) -> [%c][%c][%c][%c][%c][%c]\n", z[0], z[1], z[2], z[3], z[4], z[5]);
+	bzero_asm(z, 3);
+	printf("  bzero_asm(\"hellow\", 3) -> [%c] [%c] [%c] [%c][%c][%c]\n", z[0], z[1], z[2], z[3], z[4], z[5]);
 
     // printf("puts return value : %d | ft_puts return value : %d\n\n", ret[0], ret[1]);
     free(stri);
