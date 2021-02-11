6ff0e7aabc03448cb92a98e38c3a26b2d0b85d40
RWEngelbrecht <rigardt@brechtburg.co.za>
2021-02-11 11:21:21 +0200

    updated comment

--- libASM/bzero_asm.s
+++ libASM/bzero_asm.s
@@ -5,7 +5,7 @@ global _bzero_asm
 
 _bzero_asm:
 	mov		rcx, rsi	; copies source index into a count register, which store the loop count in iterative operations
-	xor		al, al	; eq to: mov al, 0 ;; in 64-bit mode, store al val in rdi (?)
+	xor		al, al		; eq to: mov al, 0
 	rep		stosb		; rep: repeat; stosb: copies data item from al to destination string pointed to by
 
 	ret
